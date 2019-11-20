6c21966aeabd59ea40bc8314eae57718c48934c8
Chengxuan Ying <42427938+chengsyuan@users.noreply.github.com>
2019-11-20 16:56:17 +0800

    Reorganized Project Structure

---
+++ test_case/sub_func1.c
@@ -0,0 +1,13 @@
+int cc(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j,int k){
+    return a+b+c+d+e+f+g+h+i+j+k;
+}
+
+int main(){
+    int a = cc(3,4,5,6,7,8,9,10,11,12,13);
+    return a;
+}
+
+
+
+
+
